d2bc898c0148280554f1e01dae444509423c9ff5
Debshishu Ghosh <debshishug@kitaab.biz>
2024-05-08 12:31:15 +0530

    API integration left

---
+++ services/VectorStore.py
@@ -0,0 +1,24 @@
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.vectorstores import Milvus
+from HTMLLoader import html_splitter
+
+def dbgenerator():
+    urls = ["https://help.tallysolutions.com/tally-prime/accounting/interest-calculation-tally/"]
+    splits = html_splitter(urls)
+
+    model_name = "BAAI/bge-base-en-v1.5"
+    encode_kwargs = {"normalize_embeddings": False}
+    embedding_function = HuggingFaceBgeEmbeddings(model_name=model_name, encode_kwargs=encode_kwargs)
+
+    print("hi")
+    vectorstore = Milvus.from_documents(splits,
+                                        embedding_function,
+                                        connection_args={"host": "127.0.0.1", "port": "19530"},
+                                        collection_name = "html_embeddings", ## custom collection name 
+                                        search_params = {"metric":"IP","offset":0}, ## search params
+                                        )
+    print("Done")
+    return vectorstore
+
+x = dbgenerator()
+
